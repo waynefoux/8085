2b1f2e03679f5d9ae2c779a59e9847050533b1ba
Wayne Foux <wayne.foux@gmail.com>
2022-04-14 20:43:22 -0400

    Update complements.asm

--- complements.asm
+++ complements.asm
@@ -6,7 +6,7 @@
 ; store the complement in address 2041H
 ; Code dervived from Microcomputer Experimentation with the Intel SDK-85
 
-lda 2040h       ; load accumulator direct 3A 40 20
+    lda 2040h       ; load accumulator direct 3A 40 20
     cma             ; complement data 2F
     sta 2041h       ; store accumulator direct 32 41 20
     rst 1           ; return to monitor (sdk-85 specific) CF
